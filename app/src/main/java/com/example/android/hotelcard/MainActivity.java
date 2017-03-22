086c9c2f354fdd407f2f0cb45011c03525802517
spirosd84 <spiros.drivas@gmail.com>
2017-03-22 17:43:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/hotelcard/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.hotelcard;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView link = (TextView) findViewById(R.id.button);
+
+        link.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://niki-hotel.gr/reservationform_en.php"));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
